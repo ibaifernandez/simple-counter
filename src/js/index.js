9b920edd787e9edcc060f46aa6182cd787397274
Ibai Fernández <info@ibaifernandez.com>
2023-01-25 19:30:41 -0500

    Wrapping up practice

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,35 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function Simplecounter(props) {
+    return (
+        <span className="big-counter">
+            <i class="fa-regular fa-clock white digit"></i>
+            <span id="hundredthousand" className="digit">
+                0
+            </span>
+            <span id="tenthousand" className="digit">
+                0
+            </span>
+            <span id="thousand" className="digit">
+                0
+            </span>
+            <span id="hundred" className="digit">
+                0
+            </span>
+            <span id="ten" className="digit">
+                0
+            </span>
+            <span id="unit" className="digit">
+                0
+            </span>
+            <span className="seconds">
+                <Home />
+            </span>
+        </span>
+    );
+}
+
+ReactDOM.render(<Simplecounter />, document.querySelector("#app"));
