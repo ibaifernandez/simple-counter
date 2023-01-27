b0fabe78a3265fe5f67d04c455ddd6d4114c2b9c
Ibai Fernández <info@ibaifernandez.com>
2023-01-27 18:45:02 -0500

    Wrapping up project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,113 +1,33 @@
 import React from "react";
 
-import favicon from "../../img/favicon.jpeg";
-
-// Declaration of variables
-
-let seconds = 0;
-let tenSeconds = 0;
-let hundredSeconds = 0;
-let thousandSeconds = 0;
-let tenThousandSeconds = 0;
-let hundredThousandSeconds = 0;
-
-// Creation and id-ing of new elements
-
-var counter = document.createElement("span");
-counter.id = "counter";
-
-function incrementSeconds() {
-    seconds += 1;
-    updateHundredThousands();
-    updateTenThousands();
-    updateThousands();
-    updateHundreds();
-    updateTens();
-    updateUnits();
-}
-
-function updateUnits() {
-    let unit = document.getElementById("unit");
-    unit.innerText = seconds;
-    if (seconds >= 9) {
-        seconds = -1;
-    }
-}
-
-function updateTens() {
-    let ten = document.getElementById("ten");
-    ten.innerText = tenSeconds;
-    if (seconds >= 9) {
-        tenSeconds++;
-    }
-    if (tenSeconds > 9) {
-        tenSeconds = 0;
-    }
-}
-
-function updateHundreds() {
-    let hundred = document.getElementById("hundred");
-    hundred.innerText = hundredSeconds;
-    if (seconds >= 9 && tenSeconds >= 9) {
-        hundredSeconds++;
-    }
-    if (hundredSeconds > 9) {
-        hundredSeconds = 0;
-    }
-}
-
-function updateThousands() {
-    let thousand = document.getElementById("thousand");
-    thousand.innerText = thousandSeconds;
-    if (seconds >= 9 && tenSeconds >= 9 && hundredSeconds >= 9) {
-        thousandSeconds++;
-    }
-    if (thousandSeconds > 9) {
-        thousandSeconds = 0;
-    }
-}
-
-function updateTenThousands() {
-    let tenThousand = document.getElementById("tenthousand");
-    tenThousand.innerText = tenThousandSeconds;
-    if (
-        seconds >= 9 &&
-        tenSeconds >= 9 &&
-        hundredSeconds >= 9 &&
-        thousandSeconds >= 9
-    ) {
-        tenThousandSeconds++;
-    }
-    if (tenThousandSeconds > 9) {
-        tenThousandSeconds = 0;
-    }
-}
-
-function updateHundredThousands() {
-    let hundredThousand = document.getElementById("hundredthousand");
-    hundredThousand.innerText = hundredThousandSeconds;
-    if (
-        seconds >= 9 &&
-        tenSeconds >= 9 &&
-        hundredSeconds >= 9 &&
-        thousandSeconds >= 9 &&
-        tenThousandSeconds >= 9
-    ) {
-        hundredThousandSeconds++;
-    }
-    if (hundredThousandSeconds > 9) {
-        hundredThousandSeconds = 0;
-    }
-}
-
-setInterval(incrementSeconds, 1);
-
 const Home = () => {
+    return <Simplecounter />;
+};
+
+const Simplecounter = () => {
     return (
-        <>
-            <span id="counterwatch" className="text-center"></span>
-        </>
+        <span className="big-counter">
+            <i className="far fa-clock icon"></i>
+            <span id="hundredthousand" className="digit number">
+                0
+            </span>
+            <span id="tenthousand" className="digit number">
+                0
+            </span>
+            <span id="thousand" className="digit number">
+                {thousandSeconds}
+            </span>
+            <span id="hundred" className="digit number">
+                {hundredSeconds}
+            </span>
+            <span id="ten" className="digit number">
+                {tenSeconds}
+            </span>
+            <span id="unit" className="digit number">
+                {seconds}
+            </span>
+        </span>
     );
 };
 
-export default Home;
+export { Home, Simplecounter };
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../styles/index.css";
-import Home from "./component/home.jsx";
 
-function Simplecounter(props) {
+export let seconds = 0;
+export let tenSeconds = 0;
+let hundredSeconds = 0;
+let thousandSeconds = 0;
+let tenThousandSeconds = 0;
+let hundredThousandSeconds = 0;
+
+const Simplecounter = (props) => {
     return (
         <span className="big-counter">
-            <i class="fa-regular fa-clock white digit"></i>
-            <span id="hundredthousand" className="digit">
-                0
+            <i className="far fa-clock icon"></i>
+            <span id="hundredthousand" className="digit number">
+                {hundredThousandSeconds}
             </span>
-            <span id="tenthousand" className="digit">
-                0
+            <span id="tenthousand" className="digit number">
+                {tenThousandSeconds}
             </span>
-            <span id="thousand" className="digit">
-                0
+            <span id="thousand" className="digit number">
+                {thousandSeconds}
             </span>
-            <span id="hundred" className="digit">
-                0
+            <span id="hundred" className="digit number">
+                {hundredSeconds}
             </span>
-            <span id="ten" className="digit">
-                0
+            <span id="ten" className="digit number">
+                {tenSeconds}
             </span>
-            <span id="unit" className="digit">
-                0
-            </span>
-            <span className="seconds">
-                <Home />
+            <span id="unit" className="digit number">
+                {seconds}
             </span>
         </span>
     );
-}
+};
 
-ReactDOM.render(<Simplecounter />, document.querySelector("#app"));
+const Home = (props) => {
+    return (
+        <Simplecounter
+            seconds={seconds}
+            tenSeconds={tenSeconds}
+            hundredSeconds={hundredSeconds}
+            thousandSeconds={thousandSeconds}
+            hundredThousandSeconds={hundredThousandSeconds}
+        />
+    );
+};
+
+setInterval(() => {
+    seconds++;
+
+    if (seconds > 9) {
+        seconds = 0;
+        tenSeconds++;
+    }
+
+    if (tenSeconds > 9) {
+        tenSeconds = 0;
+        hundredSeconds++;
+    }
+
+    if (hundredSeconds > 9) {
+        hundredSeconds = 0;
+        thousandSeconds++;
+    }
+
+    if (thousandSeconds > 9) {
+        thousandSeconds = 0;
+        tenThousandSeconds++;
+    }
+
+    if (tenThousandSeconds > 9) {
+        tenThousandSeconds = 0;
+        hundredThousandSeconds++;
+    }
+
+    if (hundredThousandSeconds > 9) {
+        hundredThousandSeconds = 0;
+    }
+
+    ReactDOM.render(<Home />, document.querySelector("#app"));
+}, 0.0000000000000000000000000000000000000000000000000000000000000000000000001);
